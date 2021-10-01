3e1c1849f3a7e05bd47312364a1506753211b1e7
Ignacio Rivero <igrivero03@gmail.com>
2021-10-01 00:10:16 -0300

    v2.0.1 - arreglos menores al código

--- Assets/Script/Entorno/EntrarLab.cs
+++ Assets/Script/Entorno/EntrarLab.cs
@@ -7,7 +7,7 @@ public class EntrarLab : MonoBehaviour
 {
     private void OnTriggerStay2D(Collider2D collision)
     {
-        Debug.Log("ENTRASTE LPM, PERO FUNCIONA EL IF XD");
+        //Debug.Log("el if funciona");
         if (collision.gameObject.tag == "Player" && Input.GetKeyDown(KeyCode.E))
         {
             SceneManager.LoadScene("Laboratorio");
